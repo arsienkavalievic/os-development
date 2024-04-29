d9a3286528312844885c9c26868f08f95b5065ef
Arsien Kavalievic <arsienkavalievic@gmail.com>
2024-04-29 14:06:06 +0300

    Add lab 5

---
+++ lab5/Client.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <random>
+#include <time.h>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS_GLOBALS
+#pragma warning(desable : 4996)
+#pragma warning(suppress : 4996)
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    HANDLE small_event;
+    small_event = CreateEvent(NULL, FALSE, FALSE, (LPCWSTR)"small_event");
+    WaitForSingleObject(small_event, INFINITY);
+    /*HANDLE hReadPipe, hWritePipe;
+    sscanf(argv[0], "%d", &hWritePipe);
+    sscanf(argv[1], "%d", &hReadPipe);*/
+    HANDLE hWritePipe = (HANDLE)atoi(argv[0]), hReadPipe = (HANDLE)atoi(argv[1]);
+
+    
+    LPDWORD dwBytesWrite = 0, dwBytesRead = 0;
+    int arl;
+
+    if (!ReadFile(hReadPipe, &arl, sizeof(long), dwBytesRead, NULL)) {
+        cout << "size  read is failed " << endl;char e = _getch();
+        return GetLastError();
+    }
+
+    int n, m;
+        if (!ReadFile(hReadPipe, &n, sizeof(long), dwBytesRead, NULL))
+        {
+            cout << "n read is failed " << endl;
+            return GetLastError();
+        }
+        if (!ReadFile(hReadPipe, &m, sizeof(long), dwBytesRead, NULL))
+        {
+            cout << "m read is failed " << endl;
+            return GetLastError();
+        }
+
+        
+
+    long* mas = new long[arl];
+    srand(time(NULL));
+    for (int i = 0; i < arl; i++) {
+        mas[i] = long(rand() % (m - n + 1) + n);
+    }
+
+    // Create pipes
+    for (int i = 0; i < arl; i++) {
+        if (!WriteFile(hWritePipe, &mas[i], sizeof(long), dwBytesWrite, NULL)) {
+            cout << "Write of array failed\n";
+            return GetLastError();
+        }
+    }
+
+    _cprintf("\nTo exit press any key ");
+    char t = _getch();
+
+
+    return 0;
+}
---
+++ lab5/Server.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS_GLOBALS
+#pragma warning(desable : 4996)
+#pragma warning(suppress : 4996)
+using namespace std;
+
+int main() {
+    cout << "Enter size of array: ";
+    long size; cin >> size;
+    cout << "Enter N and M: ";
+    long n, m; 
+    cin >> n >> m;
+   
+    HANDLE hWritePipe, hReadPipe;
+    HANDLE hWritePipe2, hReadPipe2;
+
+    SECURITY_ATTRIBUTES saAttr;
+    saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+    saAttr.bInheritHandle = TRUE;
+    saAttr.lpSecurityDescriptor = NULL;
+
+    // Create pipes
+    if (!CreatePipe(&hReadPipe, &hWritePipe, &saAttr, 0)) {
+        cout << "Create pipe failed" << endl;
+        return GetLastError();
+    }
+    if (!CreatePipe(&hReadPipe2, &hWritePipe2, &saAttr, 0)) {
+        cout << "Create pipe failed" << endl;
+        return GetLastError();
+    }
+    
+    HANDLE small_event;
+    small_event = CreateEvent(NULL, FALSE, FALSE, "small_event");
+
+    DWORD dwBytesWrite;
+    // Write array data to the pipe
+
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    char lpszComLine[80];
+    wsprintf(lpszComLine, (LPCSTR)"%d %d", (int)hWritePipe2, (int)hReadPipe);
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+
+    // Create Consume process
+    if (!CreateProcessA("F:\\BSU-FAMCs\\2-course\\OS\\big-brain-laba-5\\x64\\Release\\Client.exe", (LPSTR)lpszComLine,
+        NULL, NULL, TRUE,
+        CREATE_NEW_CONSOLE,
+        NULL, NULL,
+        (LPSTARTUPINFOA)&si, &pi))
+    {
+        cout << "Client.exe was not launched.\n";
+        return GetLastError();
+    }
+
+    if (!WriteFile(hWritePipe, &size, sizeof(long), &dwBytesWrite, NULL)) {
+        cout << "Write of length failed\n" << GetLastError();
+        return GetLastError();
+    }
+        if (!WriteFile(hWritePipe, &n, sizeof(long), &dwBytesWrite, NULL)) {
+            cout << "Write of array failed\n" << GetLastError();
+            return GetLastError();
+        }
+        if (!WriteFile(hWritePipe, &m, sizeof(long), &dwBytesWrite, NULL)) {
+            cout << "Write of array failed\n" << GetLastError();
+            return GetLastError();
+        }
+
+        SetEvent(small_event);
+
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    DWORD byteRwad;
+    
+    long* new_arr = new long[size];
+    for (int i = 0; i < size; i++) {
+        if (!ReadFile(hReadPipe2, &new_arr[i], sizeof(long), &byteRwad, NULL))
+        {
+            cout << "New array read is failed " << endl;
+            return GetLastError();
+        }
+    }
+
+    cout << "New array: " << endl;
+    for (int i = 0; i < size; i++) {
+        cout << new_arr[i] << " ";
+    }
+
+    CloseHandle(hReadPipe);
+    CloseHandle(hWritePipe);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    // Release the dynamically allocated memory
+    delete[] new_arr;
+
+    return 0;
+}
